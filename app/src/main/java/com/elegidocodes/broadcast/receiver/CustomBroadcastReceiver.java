af2ea8cbf23a19aba9db4e061bdc403df8d8fd3c
Fernando Canul Caballero <121654243+elegidocodes@users.noreply.github.com>
2023-11-18 19:40:35 -0600

    Custom-Receivers

--- app/src/main/java/com/elegidocodes/broadcast/receiver/CustomBroadcastReceiver.java
+++ app/src/main/java/com/elegidocodes/broadcast/receiver/CustomBroadcastReceiver.java
@@ -8,21 +8,16 @@ import android.widget.Toast;
 
 public class CustomBroadcastReceiver extends BroadcastReceiver {
 
+    static final String CUSTOM_ACTION = "com.elegidocodes.broadcast.receiver.CUSTOM_ACTION";
+
+    static final String EXTRA_TEXT = "com.elegidocodes.broadcast.receiver.EXTRA_TEXT";
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
-
-            boolean noConnectivity = intent.getBooleanExtra(
-                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
-            );
-
-            if(noConnectivity){
-                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
-            }
-
+        if(CUSTOM_ACTION.equals(intent.getAction())){
+            String receivedText = intent.getStringExtra(EXTRA_TEXT);
+            Toast.makeText(context, receivedText, Toast.LENGTH_SHORT).show();
         }
 
     }
--- app/src/main/java/com/elegidocodes/broadcast/receiver/MainActivity.java
+++ app/src/main/java/com/elegidocodes/broadcast/receiver/MainActivity.java
@@ -1,33 +1,35 @@
 package com.elegidocodes.broadcast.receiver;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private CustomBroadcastReceiver customBroadcastReceiver;
+    private CustomBroadcastReceiver customBroadcastReceiver;
 
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //customBroadcastReceiver = new CustomBroadcastReceiver();
+
+        customBroadcastReceiver = new CustomBroadcastReceiver();
+        IntentFilter filter = new IntentFilter(CustomBroadcastReceiver.CUSTOM_ACTION);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            registerReceiver(customBroadcastReceiver, filter, RECEIVER_EXPORTED);
+        }else{
+            registerReceiver(customBroadcastReceiver, filter);
+        }
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        //IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        //registerReceiver(customBroadcastReceiver, filter);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        //unregisterReceiver(customBroadcastReceiver);
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(customBroadcastReceiver);
     }
 
 }
